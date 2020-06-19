ddc37f690e6d001abee7d90fc6fa671515e9e327
younan <okokoknow@naver.com>
2020-06-19 17:09:31 +0900

    Writing Java 8 Lambda Example Code

---
+++ src/lambda.java
@@ -0,0 +1,22 @@
+public class lambda {
+    public static void main(String[] args) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Hello World.");
+            }
+        }).start();
+
+        /*
+        람다식 사용 방법.
+        1. ( parameters ) -> { expression body }
+        2. () -> { expression body }
+         */
+
+        new Thread(()-> {
+            System.out.println("Hello World.");
+        }).start();
+
+        new Thread(()-> System.out.println("Hello World")).start();
+    }
+}
