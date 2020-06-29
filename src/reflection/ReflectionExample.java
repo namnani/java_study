d4bfdbeeaaecb561f454d04515e683605ceaa696
younan <okokoknow@naver.com>
2020-06-29 23:51:39 +0900

    [java] Reflection Example

---
+++ src/reflection/ReflectionExample.java
@@ -0,0 +1,17 @@
+package reflection;
+
+import java.lang.reflect.Method;
+
+public class ReflectionExample {
+    public static void main(String[] args) {
+        try{
+            Class c = Class.forName("java.util.Stack");
+            Method m[] = c.getDeclaredMethods();
+            for(int i = 0; i<m.length; i++){
+                System.out.println(m[i].toString());
+            }
+        } catch (Throwable e) {
+            System.err.println(e);
+        }
+    }
+}
