1e209c15e36deab1585ccf44859f2404e153dac0
younan <okokoknow@naver.com>
2020-10-01 00:26:51 +0900

    [TechIO] Java 8 Stream Tutorial.

--- src/techio/java/stream/StreamExample.java
+++ src/techio/java/stream/StreamExample.java
@@ -1,6 +1,9 @@
 package techio.java.stream;
 
-
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
 
 public class StreamExample {
     public static void main(String[] args) {
@@ -12,5 +15,44 @@ public class StreamExample {
                 .map(String::toUpperCase)
                 .sorted()
                 .forEach(System.out::println);
+
+        /* Stream<String> stream = */
+        Stream.of("d2", "a2", "b1", "b3", "c")
+                .filter(s -> {
+                    System.out.println("filter: " + s);
+                    return true;
+                })
+                .forEach(s -> System.out.println("forEach: " + s));
+
+//        stream.forEach(System.out::println);
+
+        // To avoid unnecessary calls, instead of executing the operations horizontally,
+        // We can do it vertically:
+        System.out.println();
+        Stream.of("d2", "a2", "b1", "b3", "c")
+                .map(s -> {
+                    System.out.println("map: " + s);
+                    return s.toUpperCase();
+                })
+                .anyMatch(s -> {
+                    System.out.println("anyMatch: " + s);
+                    return s.startsWith("A");
+                });
+
+        // Reusing Streams
+        System.out.println();
+        Stream<String> stream =
+                Stream.of("d2", "a2", "b1", "b3", "c")
+                        .filter(s -> s.startsWith("a"));
+
+        stream.anyMatch(s -> true); // ok
+        stream.noneMatch(s -> true); // exception
+
+        Supplier<Stream<String>> streamSupplier =
+                () -> Stream.of("d2", "a2", "b1", "b3", "c")
+                        .filter(s -> s.startsWith("a"));
+
+        streamSupplier.get().anyMatch(s -> true); // ok
+        streamSupplier.get().noneMatch(s -> true); // ok
     }
 }
